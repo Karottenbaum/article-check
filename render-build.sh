507cffb75219a5422f5ebd0539c852821e5fe392
Karottenbaum <ralph.ackermann@hotmail.com>
2025-04-30 19:23:58 +0200

    upload

--- render-build.sh
+++ render-build.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 set -e
-
-# Fehlende Tools sicherstellen
 apt-get update
-apt-get install -y curl wget tar gzip firefox-esr
-
-# Neueste Geckodriver-Version holen
-GECKODRIVER_VERSION=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest \
-    | grep '"tag_name":' \
-    | sed -E 's/.*"([^"]+)".*/\1/')
-
+apt-get install -y firefox-esr
+GECKODRIVER_VERSION=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest | grep "tag_name" | sed -E 's/.*"([^"]+)".*/\1/')
 wget "https://github.com/mozilla/geckodriver/releases/download/$GECKODRIVER_VERSION/geckodriver-$GECKODRIVER_VERSION-linux64.tar.gz"
 tar -xzf "geckodriver-$GECKODRIVER_VERSION-linux64.tar.gz"
-mv geckodriver /usr/local/bin
+mv geckodriver /usr/local/bin/
 chmod +x /usr/local/bin/geckodriver
 rm "geckodriver-$GECKODRIVER_VERSION-linux64.tar.gz"
