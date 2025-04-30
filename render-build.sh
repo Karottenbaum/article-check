fb14eb5a464d4c0a22bd22c4f476825a72b10a5a
Karottenbaum <ralph.ackermann@hotmail.com>
2025-04-30 19:51:01 +0200

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,6 @@
-#!/bin/bash
-set -e
-apt-get update
-apt-get install -y firefox-esr
-GECKODRIVER_VERSION=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest | grep "tag_name" | sed -E 's/.*"([^"]+)".*/\1/')
-wget "https://github.com/mozilla/geckodriver/releases/download/$GECKODRIVER_VERSION/geckodriver-$GECKODRIVER_VERSION-linux64.tar.gz"
-tar -xzf "geckodriver-$GECKODRIVER_VERSION-linux64.tar.gz"
-mv geckodriver /usr/local/bin/
-chmod +x /usr/local/bin/geckodriver
-rm "geckodriver-$GECKODRIVER_VERSION-linux64.tar.gz"
+services:
+  - type: web
+    name: article-check
+    env: python
+    buildCommand: pip install -r requirements.txt
+    startCommand: gunicorn app:app
